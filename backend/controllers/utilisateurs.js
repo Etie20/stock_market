6eae7f3637cd5d15d21f301ff3bcf0c1e91d6e04
Etie20 <etienne.foyang@2026.ucac-icam.com>
2023-05-22 23:03:22 +0100

    api commit

--- backend/controllers/utilisateurs.js
+++ backend/controllers/utilisateurs.js
@@ -81,7 +81,7 @@ exports.login = async (req, res) => {
                 success: 0,
                 message: 'Utilisateur non trouvé !'});
         }
-        let valid = bcrypt.compare(req.body.motDePasse, utilisateur.motDePasse);
+        let valid = await bcrypt.compare(req.body.motDePasse, utilisateur.motDePasse);
         if (!valid) {
             return res.status(401).json({
                 success: 0,
