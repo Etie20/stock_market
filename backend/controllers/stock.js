478500c12e210d391fcec65d98fd3ff47fe6b59d
Etie20 <etienne.foyang@2026.ucac-icam.com>
2023-05-28 16:52:01 +0100

    api commit

--- backend/controllers/stock.js
+++ backend/controllers/stock.js
@@ -2,7 +2,7 @@ const Stock = require('../models/Stock');
 
 exports.getStockByEntrepriseId = async (req, res) => {
     try {
-        const stock = await Stock.find();
+        const stock = await Stock.find({entreprise: req.params.id});
         res.status(200).json(stock);
     } catch (e) {
         res.status(400).json({ success: 0, message: "Invalid request body" });
