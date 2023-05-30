96464bcee01521b8db6e05198154db0ef25b8521
Etie20 <etienne.foyang@2026.ucac-icam.com>
2023-05-30 12:53:10 +0100

    api commit

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ const express = require('express');
 // Importe la bibliothèque Mongoose pour interagir avec MongoDB
 const mongoose = require('mongoose');
 
+const cors = require('cors');
+
 // Établit une connexion à la base de données MongoDB hébergée sur le serveur `cluster0.unj3djg.mongodb.net` avec le nom `StockMarket`
 mongoose.connect('mongodb+srv://admin:test1234@cluster0.unj3djg.mongodb.net/StockMarket?retryWrites=true&w=majority',
     {
@@ -22,20 +24,32 @@ const marketplaceRoutes = require('./routes/marketplace');
 // Crée une instance d'application Express
 const app = express();
 
+app.use(cors());
+
+
+// Autoriser plusieurs origines d'accès
+app.use(cors({ origin: ['http://localhost:3000', 'https://stocket-market-api.onrender.com'] }));
+
+//Autoriser tous les verbes HTTP
+app.use(cors({ methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' }));
+
+// Autoriser les en-têtes personnalisés
+app.use(cors({ allowedHeaders: ['Content-Type', 'Authorization'] }));
+
+// Autoriser les cookies
+app.use(cors({ credentials: true }));
+
+
 // Utilise le middleware `express.json()` pour parser les requêtes HTTP avec des corps de requête JSON
 app.use(express.json());
+
 
 app.use('/v1/users', utilisateurRoutes);
 app.use('/v2/company', entrepriseRoutes);
 app.use('/v3/product', marchandiseRoutes);
 app.use('/v4/marketplace', marketplaceRoutes);
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+
 
 
 // Exporte l'application Express pour qu'elle puisse être utilisée dans d'autres fichiers
